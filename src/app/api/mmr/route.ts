bd9d5b2d27e728b4638fa77cf554fefa0e490e68
jameshu <james.hu@barecovetech.com>
2023-12-21 11:43:09 +0800

    feat: add cron

--- src/app/api/mmr/route.ts
+++ src/app/api/mmr/route.ts
@@ -1,11 +1,22 @@
 import { NextRequest, NextResponse } from 'next/server';
 import idList from './idList.json';
+// export async function GET(request: NextRequest) {
+//     let mmrList = [];
+//     for (const id of idList) {
+//         const data = await fetch(`https://api.opendota.com/api/players/${id}`);
+//         const mmr = await data.json();
+//         mmrList.push(mmr);
+//     }
+//     return new NextResponse(JSON.stringify(mmrList));
+// }
+
 export async function GET(request: NextRequest) {
-    let mmrList = [];
-    for (const id of idList) {
-        const data = await fetch(`https://api.opendota.com/api/players/${id}`);
-        const mmr = await data.json();
-        mmrList.push(mmr);
-    }
-    return new NextResponse(JSON.stringify(mmrList));
+    const barkBaseUrl = 'https://bark.iseayou.icu';
+    const barkKey = 'y8fhq9xYLTwBJCWQWPAgs7';
+    const title = 'mmr2';
+    const body = 'mmr';
+    const barkUrl = `${barkBaseUrl}/${barkKey}/${title}/${body}`;
+    const res = await fetch(barkUrl);
+    console.log(res.body);
+    return new NextResponse(JSON.stringify(res));
 }
